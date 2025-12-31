f4e3bd33d89e6e76681f1821f6bc9b0e9f5ed13d
rachel1w <rachelweiss@mail.tau.ac.il>
2025-12-31 13:59:49 +0200

    switch to remote db and resolve server merge conflict

--- server/server.js
+++ server/server.js
@@ -7,6 +7,25 @@ import rubberDuckRoutes from './routes/rubberDucks.js'; // Import the routes
 import mongoose from 'mongoose';
 import reportRoutes from './routes/reportRoutes.js';
 
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("MONGO_URI is missing in .env file");
+    }
+    await mongoose.connect(uri)
+    console.log("-----------------------------------------");
+    console.log("Success! Connected to MongoDB Atlas");
+    console.log("-----------------------------------------");
+  } catch (err) {
+    console.error("Connection error:", err);
+  }
+};
+
+connectDB();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
